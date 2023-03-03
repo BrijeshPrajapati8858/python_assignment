5b917bf6453b232b9b488a3835fce89d54ec6eee
BRIJESH PRAJAPATI <94066180+BrijeshPrajapati8858@users.noreply.github.com>
2023-03-03 19:47:29 +0530

    Add files via upload

---
+++ python_assignment1.py
@@ -0,0 +1,9 @@
+
+
+P = int(input("Enter the principle amount :"))
+T = int(input("Enter the time period :"))
+R = int(input("Enter the rate of interest :"))
+
+si = (P * R * T)/100
+
+print('The Simple Interest is ', si)
